23975b2ba1670f73c9581c959e4a2599df9d07ae
kokilashreerajendran <137676285+kokilashreerajendran@users.noreply.github.com>
2023-07-02 02:28:28 -0500

    Add files via upload

---
+++ day1assignments/IsPrime.java
@@ -0,0 +1,24 @@
+package week1.day1assignments;
+
+public class IsPrime {
+	public static void main(String[] args) {
+		int number = 9;	
+		int flag = 0;
+		for (int i = 2; i <= number-1; i++) {
+			if (number ==2)
+				System.out.println(number + " is prime");
+			else
+			if (number % i == 0)
+			{
+				flag = 1;
+				break;
+			}	
+		}
+		if (flag > 0)
+		System.out.println(number + " is not prime");
+		else
+			System.out.println(number + " is prime");
+		}
+				
+	}
+
