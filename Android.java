17aafc16ebc980a2272bac83bc9334e45a7ee5a1
kokilashreerajendran <137676285+kokilashreerajendran@users.noreply.github.com>
2023-07-08 08:36:23 -0500

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,5 @@
+package week3.day1interfacehomeassignment;
+
+public interface Android {
+public void openApp();
+}
---
+++ AndroidTV.java
@@ -0,0 +1,12 @@
+package week3.day1interfacehomeassignment;
+
+public abstract class AndroidTV implements Android {
+
+	public void openApp() {
+	System.out.println("Open app --> Abstract class");
+		
+	}
+
+	public abstract void playVideo(); 
+	
+}
